f0cd1977cf9247664b7d34a501cc96dd5d9397c9
sjh4616 <41767000+sjh4616@users.noreply.github.com>
2026-06-12 14:27:23 +0900

    Modify stop_process.sh to comment out commands
    Comment out directory change and docker compose command.
    
    Signed-off-by: sjh4616 <41767000+sjh4616@users.noreply.github.com>

--- scripts/stop_process.sh
+++ scripts/stop_process.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/scripts
-docker compose down || true
+# cd /home/ubuntu/scripts
+# docker compose down || true
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -q)
